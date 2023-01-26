35d131ff8688b86d884abefc5c86152a614ae6c4
Aarnav Tale <aarnav@tale.me>
2023-01-26 11:14:01 -0500

    feat: migrate to separated manifest parser and cleaned up build

--- crates/averter/build.rs
+++ crates/averter/build.rs
@@ -1,130 +1,97 @@
-use anyhow::Result;
+use manifest::load_manifest;
 use reqwest::ClientBuilder;
-use serde_json::{from_str, Value};
-use serde_yaml::from_str as from_yaml;
-use std::{
-	fs::read_to_string,
-	io::{stdout, Write},
-};
+use serde::Deserialize;
+use serde_json::from_str as from_json;
+use tokio::main;
 use vergen::{vergen, Config, ShaKind};
 
-fn main() -> Result<()> {
-	// VERGEN_BUILD_TIMESTAMP
-	// VERGEN_BUILD_SEMVER
-
-	// VERGEN_GIT_BRANCH
-	// VERGEN_GIT_SHA_SHORT
-
-	// VERGEN_RUSTC_HOST_TRIPLE
-	// VERGEN_RUSTC_LLVM_VERSION
-	// VERGEN_RUSTC_SEMVER
-
-	let mut config = Config::default();
-	*config.git_mut().sha_kind_mut() = ShaKind::Short;
-
-	vergen(config)?;
-	let manifest = load_manifest();
-	fetch_k8s_details(manifest["build"]["k8s_control_plane"].as_str().unwrap());
-
-	Ok(())
+/// Kubernetes HTTP Response
+#[derive(Deserialize)]
+struct K8sResponse {
+	#[serde(rename = "gitVersion")]
+	git_version: String,
+	platform: String,
 }
 
-fn add_config(key: &str, value: &str) {
-	let stdout = &mut stdout();
-	match writeln!(stdout, "cargo:rustc-env={}={}", key, value) {
-		Ok(_) => {}
-		Err(err) => {
-			panic!("Failed to configure config-key: {} ({})", key, err)
-		}
+fn main() {
+	register_vergen_envs();
+	let manifest = load_manifest("../../manifest.yaml");
+
+	set_env("CANISTER_PRODUCTION_NAME", &manifest.meta.production_name);
+	set_env("CANISTER_PRIVACY_ENDPOINT", &manifest.endpoints.privacy);
+	set_env("CANISTER_NOTICE_MIGRATION", &manifest.notice.migration);
+	set_env("CANISTER_CONTACT_EMAIL", &manifest.meta.contact_email);
+	set_env("CANISTER_COPYRIGHT", &manifest.meta.copyright_string);
+	set_env("CANISTER_DOCS_ENDPOINT", &manifest.endpoints.docs);
+	set_env("CANISTER_SITE_ENDPOINT", &manifest.endpoints.site);
+	set_env("CANISTER_API_ENDPOINT", &manifest.endpoints.api);
+	set_env("CANISTER_CODE_NAME", &manifest.meta.code_name);
+	set_env("CANISTER_NOTICE_DATA", &manifest.notice.data);
+	set_env("CANISTER_NOTICE_API", &manifest.notice.api);
+
+	load_k8s_info(manifest.build.k8s_control_plane);
+}
+
+/// Registers environment variables from the 'vergen' crate
+/// While unnecessary, the defaults are tuned to only what we want
+fn register_vergen_envs() {
+	let mut config = Config::default();
+
+	// Disable default features we don't need
+	*config.rustc_mut().sha_mut() = false;
+	*config.git_mut().semver_mut() = false;
+	*config.cargo_mut().enabled_mut() = false;
+	*config.rustc_mut().channel_mut() = false;
+	*config.sysinfo_mut().enabled_mut() = false;
+	*config.rustc_mut().commit_date_mut() = false;
+	*config.git_mut().commit_timestamp_mut() = false;
+
+	// Reconfigure the Git SHA to be shortened output
+	*config.git_mut().sha_kind_mut() = ShaKind::Short;
+
+	match vergen(config) {
+		Ok(_) => (),
+		Err(e) => panic!("Failed to register 'vergen' configuration ({e})"),
 	}
 }
 
-fn load_manifest() -> Value {
-	let manifest = match read_to_string("../../manifest.yaml") {
-		Ok(manifest) => manifest,
-		Err(err) => {
-			panic!("Failed to read manifest.yaml ({})", err)
-		}
-	};
-
-	let manifest: Value = from_yaml(&manifest).unwrap();
-
-	add_config(
-		"CANISTER_PRODUCT_NAME",
-		manifest["meta"]["product_name"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_CODE_NAME",
-		manifest["meta"]["code_name"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_CONTACT_EMAIL",
-		manifest["meta"]["contact_email"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_COPYRIGHT",
-		manifest["meta"]["copyright_string"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_API_ENDPOINT",
-		manifest["endpoints"]["api"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_DOCS_ENDPOINT",
-		manifest["endpoints"]["docs"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_PRIVACY_ENDPOINT",
-		manifest["endpoints"]["privacy"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_SITE_ENDPOINT",
-		manifest["endpoints"]["site"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_NOTICE_API",
-		manifest["notice"]["api"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_NOTICE_DATA",
-		manifest["notice"]["data"].as_str().unwrap(),
-	);
-
-	add_config(
-		"CANISTER_NOTICE_MIGRATION",
-		manifest["notice"]["migration"].as_str().unwrap(),
-	);
-
-	manifest
+/// Set a cargo environment variable
+/// Used for build-time variables
+fn set_env(key: &str, value: &str) {
+	println!("Registering environment variable: {key}={value}");
+	println!("cargo:rustc-env={key}={value}");
 }
 
-#[tokio::main]
-async fn fetch_k8s_details(control_plane_host: &str) {
-	let client = ClientBuilder::new()
+/// Fetches the Kubernetes version from the control plane
+/// Sets the CANISTER_K8S_VERSION environment variable
+#[main]
+async fn load_k8s_info(k8s_host: String) {
+	let client = match ClientBuilder::new()
 		.danger_accept_invalid_certs(true)
 		.build()
-		.unwrap();
+	{
+		Ok(client) => client,
+		Err(e) => panic!("Failed to build insecure HTTP client ({e})"),
+	};
 
-	let url = format!("https://{}/version", control_plane_host);
-	let response = client.get(url).send().await.unwrap();
-	let value: Value = from_str(&response.text().await.unwrap()).unwrap();
+	let url = format!("https://{k8s_host}/version");
+	let json = match client.get(url).send().await {
+		Ok(response) => match response.text().await {
+			Ok(value) => {
+				let value: K8sResponse = match from_json(&value) {
+					Ok(value) => value,
+					Err(e) => panic!("Failed to deserialize Kubernetes HTTP response ({e})"),
+				};
 
-	add_config(
+				value
+			}
+			Err(e) => panic!("Failed to parse Kubernetes HTTP response ({e})"),
+		},
+		Err(e) => panic!("Failed to fetch Kubernetes version ({e})"),
+	};
+
+	set_env(
 		"CANISTER_K8S_VERSION",
-		format!(
-			"k8s_{}-{}",
-			value["gitVersion"].as_str().unwrap(),
-			value["platform"].as_str().unwrap()
-		)
-		.as_str(),
+		format!("k8s_{}-{}", &json.git_version, &json.platform).as_str(),
 	);
 }
---
+++ crates/manifest/src/lib.rs
@@ -0,0 +1,90 @@
+use serde::Deserialize;
+use serde_yaml::from_str as from_yaml;
+use std::{
+	fs::{canonicalize, read_to_string},
+	path::Path,
+};
+
+#[warn(clippy::all)]
+#[warn(clippy::correctness)]
+#[warn(clippy::suspicious)]
+#[warn(clippy::pedantic)]
+#[warn(clippy::style)]
+#[warn(clippy::complexity)]
+#[warn(clippy::perf)]
+
+/// Strongly-typed manifest
+#[derive(Deserialize)]
+pub struct Manifest {
+	pub meta: Meta,
+	pub build: Build,
+	pub notice: Notice,
+	pub endpoints: Endpoints,
+}
+
+/// Strongly-typed meta section
+#[derive(Deserialize)]
+pub struct Meta {
+	pub code_name: String,
+	pub description: String,
+	pub contact_email: String,
+	pub production_name: String,
+	pub copyright_string: String,
+}
+
+/// Strongly-typed build section
+#[derive(Deserialize)]
+pub struct Build {
+	// pub sentry_dsn: Conditional,
+	pub k8s_control_plane: String,
+}
+
+#[derive(Deserialize)]
+pub struct Conditional {
+	pub debug: String,
+	pub release: String,
+}
+
+/// Strongly-typed notice section
+#[derive(Deserialize)]
+pub struct Notice {
+	pub api: String,
+	pub data: String,
+	pub migration: String,
+}
+
+/// Strongly-typed endpoints section
+#[derive(Deserialize)]
+pub struct Endpoints {
+	pub api: String,
+	pub docs: String,
+	pub site: String,
+	pub privacy: String,
+}
+
+/// Loads the manifest.yaml file and deserializes it
+/// Panics if the file is not found or if it fails to deserialize
+pub fn load_manifest(path: &str) -> Manifest {
+	let manifest_path = Path::new(path);
+	let manifest = match read_to_string(manifest_path) {
+		Ok(manifest) => {
+			match canonicalize(manifest_path) {
+				Ok(path) => println!("cargo:rerun-if-changed={}", path.display()),
+				Err(e) => panic!("Failed to canonicalize manifest path ({e})"),
+			};
+
+			match from_yaml(&manifest) {
+				Ok(value) => {
+					let value: Manifest = value;
+					value
+				}
+				Err(e) => panic!("Failed to parse manifest.yaml ({e})"),
+			}
+		}
+		Err(e) => {
+			panic!("Failed to read manifest.yaml ({e})")
+		}
+	};
+
+	manifest
+}
